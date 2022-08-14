edc6186c98ef5ac47336d61988734d3a28a1fb7c
Leon <geekmc2022@gmail.com>
2022-08-14 10:18:32 +0800

    upd

--- src/main/kotlin/net/geekmc/turinglib/color/ColorUtil.kt
+++ src/main/kotlin/net/geekmc/turinglib/color/ColorUtil.kt
@@ -1,23 +1,37 @@
 package net.geekmc.turinglib.color
 
-import net.geekmc.turing.Turing
 import net.geekmc.turinglib.TuringLib
 import net.geekmc.turinglib.taml.Taml
-import net.geekmc.turinglib.taml.loadAsTaml
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
-import org.yaml.snakeyaml.Yaml
-import javax.swing.text.html.HTML.Tag.I
+
+fun String.toComponent(): Component {
+    var str = this
+    // 防止&&被替换
+    str.replace("&&", "{§}")
+    for ((k, v) in ColorUtil.colorMap) {
+        str = str.replace(k, v)
+    }
+    str.replace("{§}", "&")
+    return ColorUtil.miniMessage.deserialize(str)
+}
 
 object ColorUtil {
 
-    val deserializer = MiniMessage.miniMessage()
+    val miniMessage = MiniMessage.miniMessage()
+
+    // Custom key -> MiniMessage tag
+    // &a -> <#111111> for example
+    val colorMap = mutableMapOf<String, String>()
 
     fun enable() {
 
         val taml = Taml(TuringLib.INSTANCE.getResource("CustomColors.yml")!!)
         val colors: List<String> = taml["colors", listOf()]
-
-
+        for (str in colors) {
+            val split = str.split("@")
+            colorMap[split[0]] = "<${split[1]}>"
+        }
 
     }
 
--- src/main/kotlin/net/geekmc/turinglib/color/PlayerExtender.kt
+++ src/main/kotlin/net/geekmc/turinglib/color/PlayerExtender.kt
@@ -1,8 +1,7 @@
 package net.geekmc.turinglib.color
 
 import net.minestom.server.command.CommandSender
-import net.minestom.server.entity.Player
 
 fun CommandSender.send(message:String) {
-    sendMessage(ColorUtil.deserializer.deserialize(message))
+    sendMessage(ColorUtil.miniMessage.deserialize(message))
 }
